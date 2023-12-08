81ccc9bf85c74f9ac23259dd6cd94a440efb67e2
sfomuseumbot <sfomuseumbot@localhost>
2023-12-08 13:24:01 -0800

    update to read metadata from static.sfom

--- cmd/pdf/main.go
+++ cmd/pdf/main.go
@@ -3,42 +3,68 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
 
 	"github.com/go-pdf/fpdf"
 	"github.com/sfomuseum/go-sfomuseum-colouringbook"
+	"github.com/whosonfirst/go-reader"
+	_ "github.com/whosonfirst/go-reader-http"
+	wof_reader "github.com/whosonfirst/go-whosonfirst-reader"
+	"github.com/tidwall/gjson"
 )
 
 func main() {
 
 	var image string
-
+	var object_id int64
+	var reader_uri string
+	
 	flag.StringVar(&image, "image", "", "...")
-
+	flag.Int64Var(&object_id, "object-id", 0, "...")	
+	flag.StringVar(&reader_uri, "reader-uri", "https://static.sfomuseum.org/data/", "...")
+	
 	flag.Parse()
 
 	ctx := context.Background()
 
+	r, err := reader.NewReader(ctx, reader_uri)
+
+	if err != nil {
+		log.Fatalf("Failed to create reader, %v", err)
+	}
+		
+	// Create PDF
+	
 	pdf := fpdf.New("L", "in", "Letter", "")
 
-	//
+	// Something something something get all the images for object here...
+	
+	// Get object metadata
 
-	url := "https://collection.sfomuseum.org/objects/1511943901/"
-	title := "negative: San Francisco Airport, Terminal Building construction"
-	date := "1952"
-	creditline := "Transfer from San Francisco International Airport"
-	accnum := "2011.032.0303"
+	body, err := wof_reader.LoadBytes(ctx, r, object_id)
 
-	sheet_opts := &colouringbook.AddSheetOptions{
-		Image:           image,
-		Title:           title,
-		Date:            date,
-		CreditLine:      creditline,
-		AccessionNumber: accnum,
-		URL:             url,
+	if err != nil {
+		log.Fatalf("Failed to load feature for object, %v", err)
 	}
 
-	err := colouringbook.AddSheet(ctx, pdf, sheet_opts)
+	title_rsp := gjson.GetBytes(body, "properties.wof:name")
+	date_rsp := gjson.GetBytes(body, "properties.sfomuseum:date")
+	creditline_rsp := gjson.GetBytes(body, "properties.sfomuseum:creditline")
+	accession_number_rsp := gjson.GetBytes(body, "properties.sfomuseum:accession_number")	
+
+	url := fmt.Sprintf("https://collection.sfomuseum.org/objects/%d/", object_id)
+	
+	sheet_opts := &colouringbook.AddSheetOptions{
+		Image:           image,
+		URL:             url,		
+		Title:           title_rsp.String(),
+		Date:            date_rsp.String(),
+		CreditLine:      creditline_rsp.String(),
+		AccessionNumber: accession_number_rsp.String(),
+	}
+
+	err = colouringbook.AddSheet(ctx, pdf, sheet_opts)
 
 	if err != nil {
 		log.Fatalf("Failed to add sheet, %v", err)
--- go.mod
+++ go.mod
@@ -5,6 +5,21 @@ go 1.21.5
 require (
 	github.com/boombuler/barcode v1.0.1
 	github.com/go-pdf/fpdf v0.9.0
+	github.com/tidwall/gjson v1.14.1
+	github.com/whosonfirst/go-reader v1.0.2
+	github.com/whosonfirst/go-reader-http v0.3.1
+	github.com/whosonfirst/go-whosonfirst-reader v1.0.1
 )
 
-require github.com/ruudk/golang-pdf417 v0.0.0-20201230142125-a7e3863a1245 // indirect
+require (
+	github.com/aaronland/go-roster v1.0.0 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/paulmach/orb v0.7.0 // indirect
+	github.com/ruudk/golang-pdf417 v0.0.0-20201230142125-a7e3863a1245 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	github.com/whosonfirst/go-ioutil v1.0.2 // indirect
+	github.com/whosonfirst/go-whosonfirst-sources v0.1.0 // indirect
+	github.com/whosonfirst/go-whosonfirst-uri v1.2.0 // indirect
+)
